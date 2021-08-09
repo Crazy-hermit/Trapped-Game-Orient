abd6003d4f054815a5b8e7e7199ed0598fe135c5
itsyahboijim <itsyahboijim@github.com>
2021-08-09 19:09:46 +0800

    Modify base AI scripts

--- Trapped (Orient)/Assets/Codes/AI/EnemyAI.cs
+++ Trapped (Orient)/Assets/Codes/AI/EnemyAI.cs
@@ -7,6 +7,6 @@ public abstract class EnemyAI : MonoBehaviour
     //Continuously move towards the player, with variations on movement speed
     protected void MoveToPlayer(float moveSpeed)
     {
-        transform.position = Vector2.MoveTowards(transform.position, PlayerManager.instance.playerPosition, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, PlayerManager.instance.playerPosition, Time.deltaTime * moveSpeed);
     }
 }
--- Trapped (Orient)/Assets/Codes/AI/ProximityAI.cs
+++ Trapped (Orient)/Assets/Codes/AI/ProximityAI.cs
@@ -12,6 +12,11 @@ public class ProximityAI : EnemyAI
 
     private Rigidbody2D rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         //Check if player is within specified enemy range by using distance formula
@@ -19,6 +24,10 @@ public class ProximityAI : EnemyAI
         {
             StopCoroutine(moveCor);
             MoveToPlayer(moveSpeed);
+        }
+        else
+        {
+            patrol();
         }
     }
 
@@ -28,10 +37,16 @@ public class ProximityAI : EnemyAI
         moveCor = StartCoroutine(move());
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, enemyRange);
+    }
+
     public IEnumerator move()
     {
         //Move left for 3 secs, move right for 3 secs, stowp
-        rb.velocity = new Vector2(-moveSpeed, 0f);
+        rb.velocity = new Vector2((-moveSpeed), 0f);
         yield return new WaitForSeconds(3f);
         rb.velocity = new Vector2(moveSpeed, 0f);
         yield return new WaitForSeconds(3f);
